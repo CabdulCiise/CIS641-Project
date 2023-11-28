18cb9278eb922ab639b720fea2d4a850b54bd49b
Cabdul Ciise <cabdul.ciise@elektron-solutions.com>
2023-11-28 11:11:02 -0500

    Updated scaffolded vue project.

--- src/vue-project/src/views/HomeView.vue
+++ src/vue-project/src/views/HomeView.vue
@@ -1,9 +1,27 @@
+<template>
+    <Document />
+    <!-- <Login @logged-in="onLoggedIn" v-if="!user"/> -->
+    <!-- <Register @user-registered="onRegistered" v-else="true" /> -->
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+import Document from '../components/Document.vue'
+// import Login from '../components/Login.vue'
+// import Register from '../components/Register.vue';
+
+const user = ref(null)
+
+function onLoggedIn(newUser) {
+    user = newUser
+}
+
+function onLoggedOut() {
+    user = null
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+</style>
