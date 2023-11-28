5904b1b040d96621f2bfb5cd1f0aad15f755656c
Cabdul Ciise <cabdul.ciise@elektron-solutions.com>
2023-11-28 11:11:28 -0500

    Added new vue pages and components.

---
+++ src/vue-project/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row h-100 d-flex justify-content-center align-items-center">
+        <form @submit.prevent="login" class="col-5">
+            <h1 class="mb-3">LOG IN</h1>
+            <input type="input" class="form-control mb-3" placeholder="Enter Username" required autofocus v-model="username">
+            <input type="password" class="form-control mb-3" placeholder="Password" required v-model="password">
+            <button type="submit" class="btn btn-lg btn-primary col-12">Sign in</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            axios.post('http://localhost:5000/login', {
+                'username': this.username,
+                'password': this.password
+            })
+            .then((res) => {
+                console.log(res.data)
+                this.$emit('logged-in', res.data)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/vue-project/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="row h-100 d-flex justify-content-center align-items-center">
+        <form @submit.prevent="register" class="col-5">
+            <h1 class="mb-3">CREATE AN ACCOUNT</h1>
+            <input type="input" class="form-control mb-3" placeholder="Enter Username" required autofocus v-model="username">
+            <input type="password" class="form-control mb-3" placeholder="Password" required v-model="password">
+            <input type="password" class="form-control" placeholder="Repeat your password" required v-model="passwordRepeat">
+            <button type="submit" class="btn btn-lg btn-primary col-12 mt-3">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            passwordRepeat: ''
+        }
+    },
+    methods: {
+        register() {
+            axios.post('http://localhost:5000/register', {
+                'username': this.username,
+                'password': this.password
+            })
+            .then((res) => {
+                console.log(res.data)
+                this.$emit('user-registered', res.data)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
